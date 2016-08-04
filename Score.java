b814bee70ee31c86bcb21bd3f062424c0cdcb3af
Pushpinder Singh <pushpisingh@paypal.com>
2016-08-04 17:20:27 +0530

    Added skeleton class

---
+++ Score.java
@@ -0,0 +1,30 @@
+public class Score {
+
+	int[] points = { 0, 0 };
+	int[] sets = { 0, 0 };
+	int[] games = { 0, 0 };
+
+	private boolean hasWon() {
+		return false;
+	}
+
+	public void incrementPoint(int player) {
+
+	}
+
+	private void incrementGames(int player) {
+
+	}
+
+	private void incrementSets(int player) {
+
+	}
+
+	public void resetPoints() {
+
+	}
+
+	public void resetGames() {
+
+	}
+}
